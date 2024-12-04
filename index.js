2f1ba31ece7618f3d3ac0f485c511c35c9ee1dd0
Derebeytullah <derebeytullahdere@gmail.com>
2024-12-04 09:26:05 +0300

    user interface

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 let header = document.getElementById('headercontent');
 let footer = document.getElementById('footercontent');
 
-
-
-
-
+// Header'ı yüklemek için asenkron fonksiyon
 const headerfile = () => {
-    fetch('/pages/header.html')
-    .then(response => response.text())
-    .then(html =>{
-        header.innerHTML = html;
-    })
-}
-headerfile();
+    return fetch('/pages/header.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Header dosyası bulunamadı');
+            }
+            return response.text();
+        })
+        .then(html => {
+            header.innerHTML = html;
+            console.log("Header yüklendi");
+        });
+};
 
+// Footer'ı yüklemek için asenkron fonksiyon
 const footerfile = () => {
-    fetch('/pages/footer.html')
-    .then(response => response.text())
-    .then(html =>{
-        footer.innerHTML = html;
-    })
-}
-footerfile();
+    return fetch('/pages/footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Footer dosyası bulunamadı');
+            }
+            return response.text();
+        })
+        .then(html => {
+            footer.innerHTML = html;
+            console.log("Footer yüklendi");
+        });
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    let loginarea = document.querySelector('.login-area');
-    console.log(loginarea);
-    let logininfo = false;
-    if (loginarea) {
-        console.log(logininfo);
-        loginarea.innerHTML = 
-        `
-        <li><a class="login-btn" href="">Kayıt Ol <i class="fa-solid fa-user-plus"></i></a></li>
-        <li><a class="login-btn" href="/pages/log-in.html">Giriş Yap <i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
-        `;
-    }
-});
+// Her ikisini de yükleyip, login-area'yı eklemek için tüm işlemi bekleyin
+Promise.all([headerfile(), footerfile()])
+    .then(() => {
+        let loginarea = document.querySelector('.login-area');
+        let logininfo = false;
+        console.log("Login-area öğesi:", loginarea);
+
+        if (logininfo == false) {
+            console.log("Login-area bulundu.");
+            loginarea.innerHTML = `
+                <li><a class="login-btn" href="">Kayıt Ol <i class="fa-solid fa-user-plus"></i></a></li>
+                <li><a class="login-btn" href="/pages/log-in.html">Giriş Yap <i class="fa-solid fa-arrow-right-to-bracket"></i></a></li>
+            `;
+        }
+    })
+    .catch(err => {
+        console.error('Header veya Footer yüklenirken hata oluştu:', err);
+    });
